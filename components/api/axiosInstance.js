e434963a7b70d407af155afa58602c2505e525ec
shkTalha33 <shykhtalha33@gmail.com>
2025-12-09 01:00:45 +0500

    fix api for calling twice

--- components/api/axiosInstance.js
+++ components/api/axiosInstance.js
@@ -1,13 +1,16 @@
-// Updated axiosInstance.js - Simple version
+// Updated axiosInstance.js - With request deduplication
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const axiosInstance = axios.create({
+const baseAxios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
+// Map to store pending GET requests to prevent duplicates
+const pendingRequests = new Map();
+
 // Request interceptor - add token to requests
-axiosInstance.interceptors.request.use(
+baseAxios.interceptors.request.use(
   (config) => {
     let token;
       // Try to get from cookies first
@@ -32,4 +35,40 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-// No need to change response interceptor if you don't want to
+// Create a wrapper for axios instance with deduplication for GET requests
+export const axiosInstance = {
+  ...baseAxios,
+  
+  get: (url, config = {}) => {
+    const requestKey = `GET-${url}`;
+    
+    // If this exact GET request is already pending, return the existing promise
+    if (pendingRequests.has(requestKey)) {
+      return pendingRequests.get(requestKey);
+    }
+    
+    // Create a new request promise
+    const requestPromise = baseAxios.get(url, config)
+      .then((response) => {
+        // Clean up the pending request after it completes
+        pendingRequests.delete(requestKey);
+        return response;
+      })
+      .catch((error) => {
+        // Clean up the pending request on error
+        pendingRequests.delete(requestKey);
+        throw error;
+      });
+    
+    // Store the promise for potential duplicate requests
+    pendingRequests.set(requestKey, requestPromise);
+    
+    return requestPromise;
+  },
+  
+  // For POST, PUT, DELETE - use the base axios without deduplication
+  post: (url, data, config) => baseAxios.post(url, data, config),
+  put: (url, data, config) => baseAxios.put(url, data, config),
+  delete: (url, config) => baseAxios.delete(url, config),
+  patch: (url, data, config) => baseAxios.patch(url, data, config),
+};
